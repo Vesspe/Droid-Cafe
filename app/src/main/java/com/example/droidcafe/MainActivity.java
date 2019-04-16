2838b1f584f21e1b6940c0fc77e5ecfd09b68cb8
Vesspe <vesspe77@gmail.com>
2019-04-16 16:24:49 +0200

    Added up button

--- app/src/main/java/com/example/droidcafe/MainActivity.java
+++ app/src/main/java/com/example/droidcafe/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.droidcafe;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,7 +27,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                String data = getString(R.string.google_mtw_coord_zoom12);
+                intent.setData(Uri.parse(data));
+                if(intent.resolveActivity(getPackageManager())!=null)
+                    startActivity(intent);
+                Snackbar.make(view, R.string.snackbar_text, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -49,11 +56,23 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId())
+        {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+                break;
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+                break;
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+                break;
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+                break;
+            default:
+                // do nothing for now
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -86,4 +105,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, OrderActivity.class);
         startActivity(intent);
     }
+
 }
